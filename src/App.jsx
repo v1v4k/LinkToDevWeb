ac5ad9305492415b35eaf57f1e35b964217fde98
v1v4k <viivekreddi@outlook.com>
2024-11-15 11:23:33 -0600

    Created Routing for Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
+import Feed from "./Feed"
 
 const App = () => {
   return (
     <>
-      <Body />
+      <BrowserRouter basename="/">
+        <Routes>
+            <Route path='/' element={<Body />} >
+              <Route path='/login' element={<Login />} />
+              <Route path='/profile' element={<Profile />}/>
+              <Route path='/feed' element={<Feed />} />
+            </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom"
 import Navbar from "./NavBar"
+import Footer from "./Footer"
 
 const Body = () => {
   return (
     <div>
         <Navbar />
+        <Outlet />
+        <Footer />
     </div>
   )
 }
